3e15a0a8f1254b9843305e6580e25007c4176337
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-08-12 17:17:35 +0600

    Learn very basic in python.

---
+++ Module-1/test.py
@@ -0,0 +1,15 @@
+# Condition in python
+number = int (input())
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# List in python
+mylist = [1, 2, 3, 4, 5]
+print(mylist)
+
+# Dictionary in python
+mydict = {"name": "Rakeeb Hasan", "age": 24, "city": "Sherpur"}
+print(mydict)
